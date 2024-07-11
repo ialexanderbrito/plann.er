ad676faa4af76229966842a3b87f0798dc89df41
ialexanderbrito <xanderzinho26@gmail.com>
2024-07-10 23:56:59 -0300

    feat: add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import { App } from '@/app';
+import { Toaster } from '@/components/ui/toaster';
+
 import '@/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>,
 );
